6636711cb7a4ae9ce04af843973a5de339f5ebde
frank_wjs@hotmail.com <frank_wjs@hotmail.com>
2020-07-10 02:37:29 +0800

    :sparkles: Introducing new features. 手撕红黑树代码（java11）

---
+++ Week_03/src/com/algorithm/common/rbtree/RBTreeNode.java
@@ -0,0 +1,97 @@
+package com.algorithm.common.rbtree;
+
+/**
+ * 红黑树特点
+ *
+ * <p>1. 每节点要么是黑色，要么是红色
+ *
+ * <p>2. 根节点是黑色
+ *
+ * <p>3. 每个叶子节点（NIL）是黑色
+ *
+ * <p>4. 每个红色节点的两个子节点和父节点一定都是黑色（从每个叶子节点到根节点的所有路径上不能有两个连续的红色节点）
+ *
+ * <p>5. 任意一节点到每个叶子节点的路径都包含数量相同的黑色节点（黑高） 从5又可推理出
+ *
+ * <p>6. 如果一个节点存在黑色子节点，那么该节点肯定有两个子结点
+ *
+ * @author frank woo(吴峻申) <br>
+ *     email:<a href="mailto:frank_wjs@hotmail.com">frank_wjs@hotmail.com</a> <br>
+ * @date 2020/7/8 17:28<br>
+ */
+public class RBTreeNode<T extends Comparable<T>> {
+  /** 节点值 */
+  private T value;
+  /** 父节点指针 */
+  private RBTreeNode<T> parent;
+  /** 左子树指针 */
+  private RBTreeNode<T> left;
+  /** 右子树指针 */
+  private RBTreeNode<T> right;
+
+  /** 节点颜色是红还是黑（非红） */
+  private boolean isRed;
+
+  public RBTreeNode() {}
+
+  public RBTreeNode(T value) {
+    this.value = value;
+  }
+
+  public T getValue() {
+    return value;
+  }
+
+  void setValue(T value) {
+    this.value = value;
+  }
+
+  RBTreeNode<T> getLeft() {
+    return left;
+  }
+
+  void setLeft(RBTreeNode<T> left) {
+    this.left = left;
+  }
+
+  RBTreeNode<T> getRight() {
+    return right;
+  }
+
+  void setRight(RBTreeNode<T> right) {
+    this.right = right;
+  }
+
+  RBTreeNode<T> getParent() {
+    return parent;
+  }
+
+  void setParent(RBTreeNode<T> parent) {
+    this.parent = parent;
+  }
+
+  public boolean isRed() {
+    return isRed;
+  }
+
+  public void setRed(boolean red) {
+    isRed = red;
+  }
+
+  boolean isBlack() {
+    return !isRed;
+  }
+
+  void makeRed() {
+    isRed = true;
+  }
+
+  void makeBlack() {
+    isRed = false;
+  }
+
+  @Override
+  public String toString() {
+    return value.toString();
+  }
+}
---
+++ Week_03/src/com/algorithm/common/rbtree/RBTreeRunner.java
@@ -0,0 +1,92 @@
+package com.algorithm.common.rbtree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author frank woo(吴峻申) <br>
+ *     email:<a href="mailto:frank_wjs@hotmail.com">frank_wjs@hotmail.com</a> <br>
+ * @date 2020/7/10 01:33<br>
+ */
+public class RBTreeRunner {
+  public static void main(String[] args) {
+    RBTree<String> bst = new RBTree<>();
+
+    bst.addNode(new RBTreeNode<>("d"));
+    bst.addNode(new RBTreeNode<>("c"));
+    bst.addNode(new RBTreeNode<>("b"));
+    bst.addNode(new RBTreeNode<>("f"));
+    bst.addNode(new RBTreeNode<>("a"));
+    bst.addNode(new RBTreeNode<>("e"));
+    bst.addNode(new RBTreeNode<>("g"));
+    bst.addNode(new RBTreeNode<>("h"));
+
+    System.out.println("\n====================================================================");
+    printTree(bst.getRoot());
+    System.out.println("\n一共" + bst.getSize() + "个节点");
+    // c(黑;;)
+    // b(黑;c;左)	e(红;c;右)
+    // a(红;b;左)	d(黑;e;左)	g(黑;e;右)
+    // f(红;g;左)	h(红;g;右)
+
+    System.out.println("\n====================================================================");
+    bst.remove(new RBTreeNode<>("c"));
+//    System.out.println("删除节点值：" + value);
+
+    printTree(bst.getRoot());
+    System.out.println("\n一共" + bst.getSize() + "个节点");
+    // d(黑;;)
+    // b(黑;d;左)	g(红;d;右)
+    // a(红;b;左)	e(黑;g;左)	h(黑;g;右)
+    // f(红;e;右)
+
+    System.out.println("\n====================================================================");
+    RBTreeNode<String> node = bst.find(new RBTreeNode<>("c"));
+    if (node != null) {
+      System.out.println("节点值: " + node.getValue() + ",节点颜色: " + (node.isBlack() ? "黑" : "红"));
+    } else {
+      System.out.println("Nil节点");
+    }
+
+    System.out.println("\n====================================================================");
+    node = bst.find(new RBTreeNode<>("f"));
+    System.out.println("节点值: " + node.getValue() + ",节点颜色: " + (node.isBlack() ? "黑" : "红"));
+  }
+
+  /**
+   * 打印红黑树，树中每层都在一行显示
+   *
+   * @param root 根节点
+   */
+  private static void printTree(RBTreeNode<String> root) {
+    Queue<RBTreeNode<String>> queue = new LinkedList<>();
+    Queue<RBTreeNode<String>> queue2 = new LinkedList<>();
+    if (root == null) {
+      return;
+    }
+    queue.add(root);
+    boolean firstQueue = true;
+
+    while (!queue.isEmpty() || !queue2.isEmpty()) {
+      Queue<RBTreeNode<String>> q = firstQueue ? queue : queue2;
+      RBTreeNode<String> node = q.poll();
+
+      if (node != null) {
+        RBTreeNode<String> parentNode = node.getParent();
+        String position = parentNode == null ? "" : node == parentNode.getLeft() ? "左" : "右";
+        String parentNodeStr = parentNode == null ? "" : parentNode.toString();
+        String color = node.isRed() ? "红" : "黑";
+        System.out.print(node + "(" + color + ";" + parentNodeStr + ";" + position + ")\t");
+        if (node.getLeft() != null) {
+          (firstQueue ? queue2 : queue).add(node.getLeft());
+        }
+        if (node.getRight() != null) {
+          (firstQueue ? queue2 : queue).add(node.getRight());
+        }
+      } else {
+        System.out.println();
+        firstQueue = !firstQueue;
+      }
+    }
+  }
+}
