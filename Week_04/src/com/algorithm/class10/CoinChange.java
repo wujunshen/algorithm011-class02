326e9d3d62c0635054fdacb83409b17df40dd7be
frank_wjs@hotmail.com <frank_wjs@hotmail.com>
2020-07-13 11:10:50 +0800

    :bento: Adding or updating assets. 第四周作业和小结（未完待续）

---
+++ Week_04/src/com/algorithm/class10/CoinChange.java
@@ -0,0 +1,25 @@
+package com.algorithm.class10;
+
+/**
+ * 322. 零钱兑换
+ *
+ * <p>给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+ *
+ * <p>
+ *
+ * <p>示例 1:
+ *
+ * <p>输入: coins = [1, 2, 5], amount = 11 输出: 3 解释: 11 = 5 + 5 + 1 示例 2:
+ *
+ * <p>输入: coins = [2], amount = 3 输出: -1
+ *
+ * <p>说明: 你可以认为每种硬币的数量是无限的。
+ *
+ * <p>来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/coin-change
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author frank woo(吴峻申) <br>
+ *     email:<a href="mailto:frank_wjs@hotmail.com">frank_wjs@hotmail.com</a> <br>
+ * @date 2020/7/13 11:00<br>
+ */
+public class CoinChange {}
---
+++ Week_04/src/com/algorithm/homework/hard/JumpGameII.java
@@ -0,0 +1,26 @@
+package com.algorithm.homework.hard;
+
+/**
+ * 45. 跳跃游戏 II
+ *
+ * <p>给定一个非负整数数组，你最初位于数组的第一个位置。
+ *
+ * <p>数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ *
+ * <p>你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+ *
+ * <p>示例:
+ *
+ * <p>输入: [2,3,1,1,4] 输出: 2 解释: 跳到最后一个位置的最小跳跃数是 2。   从下标为 0 跳到下标为 1 的位置，跳 1 步，然后跳 3 步到达数组的最后一个位置。
+ * 说明:
+ *
+ * <p>假设你总是可以到达数组的最后一个位置。
+ *
+ * <p>来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/jump-game-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author frank woo(吴峻申) <br>
+ *     email:<a href="mailto:frank_wjs@hotmail.com">frank_wjs@hotmail.com</a> <br>
+ * @date 2020/7/13 11:09<br>
+ */
+public class JumpGameII {}
