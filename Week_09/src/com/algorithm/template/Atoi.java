22a69b2e5657852697635b08ed2d61a338d6eae9
frank_wjs@hotmail.com <frank_wjs@hotmail.com>
2020-08-22 18:09:58 +0800

    :bento: Adding or updating assets. 第九周作业和小结（未完待续）

--- Week_09/src/com/algorithm/template/Atoi.java
+++ Week_09/src/com/algorithm/template/Atoi.java
@@ -43,9 +43,37 @@ public class Atoi {
     return total * sign;
   }
 
+  /**
+   * 输入: "42" 输出: 42 示例 2:
+   *
+   * <p>输入: " -42" 输出: -42 解释: 第一个非空白字符为 '-', 它是一个负号。   我们尽可能将负号与后面所有连续出现的数字组合起来，最后得到 -42 。 示例 3:
+   *
+   * <p>输入: "4193 with words" 输出: 4193 解释: 转换截止于数字 '3' ，因为它的下一个字符不为数字。 示例 4:
+   *
+   * <p>输入: "words and 987" 输出: 0 解释: 第一个非空字符是 'w', 但它不是数字或正、负号。 因此无法执行有效的转换。 示例 5:
+   *
+   * <p>输入: "-91283472332" 输出: -2147483648 解释: 数字 "-91283472332" 超过 32 位有符号整数范围。   因此返回 INT_MIN
+   * (−231) 。
+   *
+   * <p>来源：力扣（LeetCode） 链接：https://leetcode-cn.com/problems/string-to-integer-atoi
+   * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+   *
+   * @param args
+   */
   public static void main(String[] args) {
-    String str = "10908asd0hiqjde3lq9l";
+    String str = "42";
+    System.out.println("value is:" + myAtoi(str));
 
+    str = " -42";
+    System.out.println("value is:" + myAtoi(str));
+
+    str = "4193 with words";
+    System.out.println("value is:" + myAtoi(str));
+
+    str = "words and 987";
+    System.out.println("value is:" + myAtoi(str));
+
+    str = "-91283472332";
     System.out.println("value is:" + myAtoi(str));
   }
 }
